549629e019ee89eb66b7f7364c6a5fe0b8c4a9ed
Guillem Mauri <guimaujim@gmail.com>
2017-03-06 12:46:27 +0100

    End of class

--- sobres/sobres/urls.py
+++ sobres/sobres/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from isobres.views import *
+
 urlpatterns = [
+    url(r'^$', homepage),
     url(r'^admin/', include(admin.site.urls)),
 ]
